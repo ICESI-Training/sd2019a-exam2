306fa9a92e648906e5f5ca377dd3076811955b9f
danielperga97 <danielperga97@outlook.com>
2019-04-24 09:56:40 -0500

    update code

--- backend/code/app.py
+++ backend/code/app.py
@@ -1,14 +1,32 @@
 from flask import Flask, request
-
+import redis
+key=0
 app = Flask(__name__)
-
-@app.route('/result', methods = ['GET', 'POST'])
+r= redis.Redis(host='localhost', port= 6379 , db=0)
+@app.route('/result', methods = ['GET'])
 def result():
     if request.method == 'GET':
-        place = request.args.get('place', None)
-        if place:
-            return place
+        param = request.args.get('place', None)
+        if param:
+            r.get(param)
+            return r.get(param)
         return "No place information is given"
+
+@app.route('/insert', methods= ['POST'])
+def insert():
+    i1= "entro al metodo" 
+    if request.method == 'POST':
+        i1 += "entró al post"
+        param = request.form
+        i1+= str(param)
+        print(param)
+        if param:
+            i1+= "entro al condicional"
+            return i1
+            r.set(key,param)
+            key=str(key+1)
+            return "Se ha agregado la entrada a la base de datos"
+        return i1
 
 if __name__ == '__main__':
     app.run("0.0.0.0",5000)
