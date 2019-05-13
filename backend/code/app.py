579321933e0c8357dd499aef010392be3b5106c6
danielperga97 <danielperga97@outlook.com>
2019-05-13 17:37:09 -0500

    add app and logging

--- backend/code/app.py
+++ backend/code/app.py
@@ -1,5 +1,22 @@
 from flask import Flask, request
 import redis
+import logging
+from fluent import handler
+
+custom_format = {
+  'host': '%(hostname)s',
+  'where': '%(module)s.%(funcName)s',
+  'type': '%(levelname)s',
+  'stack_trace': '%(exc_text)s'
+}
+
+logging.basicConfig(level=logging.INFO)
+l = logging.getLogger('app.logger')
+h = handler.FluentHandler('app.follow', host='192.168.130.180', port=24224)
+formatter = handler.FluentRecordFormatter(custom_format)
+h.setFormatter(formatter)
+l.addHandler(h)
+
 
 app = Flask(__name__)
 r= redis.Redis(host='192.168.130.180', port= 6379 , db=0)
@@ -9,7 +26,9 @@ def result():
         param = request.args.get('search', None)
         if param:
             r.get(param)
+            l.info({  'Operation': 'query', 'result': 'successful'})
             return r.get(param)
+        l.error({  'Operation': 'query', 'result': 'failed'})
         return "No place information is given"
 
 @app.route('/insert', methods= ['POST'])
@@ -19,7 +38,9 @@ def insert():
         value = request.form['value']
         if key and value:
             r.set(key,value)
+            l.info({  'Operation': 'insert', 'result': 'successful'})
             return "Se ha agregado:  llave= "+str(key)+" value= "+str(value)
+        l.error({  'Operation': 'insert', 'result': 'failed'})
         return "error"
 
 if __name__ == '__main__':
